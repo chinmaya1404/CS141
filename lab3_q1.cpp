cea9997f0d90df8819378e41acfc5e270bd1c918
chinmaya1404 <42134353+chinmaya1404@users.noreply.github.com>
2018-08-17 11:17:51 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,24 @@
+// include the library
+#include <iostream>
+using namespace std;
+
+// write main function
+int main()
+{
+// declaration of variables
+
+int a1;
+float a2;
+double a3;
+char a4;
+bool a5;
+// output values of the basic datatype
+
+cout << "SIZE OF INTEGER DATATYPE IS "<<sizeof(a1)<<"BYTE"<<endl;
+cout << "SIZE OF FLOAT DATATYPE IS "<<sizeof(a2)<<"BYTE"<<endl;
+cout << "SIZE OF DOUBLE DATATYPE IS "<<sizeof(a3)<<"BYTE"<<endl;
+cout << "SIZE OF CHARACTER DATATYPE IS "<<sizeof(a4)<<"BYTE"<<endl;
+cout << "SIZE OF BOOLEAN DATATYPE IS "<<sizeof(a5)<<"BYTE"<<endl;
+// end function
+return 0;
+}
